22bae3b0dd9afd4d23222badbee7692c64a531a7
Victor-Valery <81595723+Victor-Valery@users.noreply.github.com>
2021-06-27 00:58:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BA_C_sharp_9__try_catch__brak_continue__arrays___
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. TRY-CATCH  -- detect errors
+            // 2. BREAK-CONTINUE
+            // 3. ARRAYS
+
+            Console.WriteLine("Bir tamsayi deger giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+            Console.WriteLine(sayi);
+
+        }
+    }
+}
